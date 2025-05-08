6087a0a1f688dc87cb28f4fc911987e92e08a39a
jonatasmedeiros155 <jonatasmedeiros155@gmail.com>
2025-05-08 15:21:53 -0300

    feat: add validaçoes a sales-order-header e a customer

--- srv/main.ts
+++ srv/main.ts
@@ -1,5 +1,6 @@
-import { ResultsHandler, Service } from '@sap/cds'
-import { Customers } from '@cds-models/sales';
+import cds, { Request , Service } from '@sap/cds'
+import { Customers, SalesOrderHeaders } from '@cds-models/sales';
+
 
 export default (service: Service) => {
     service.after('READ', 'Customers',(results: Customers) => {
@@ -9,6 +10,19 @@ export default (service: Service) => {
             }
         })
     });
+    service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
+        const params = request.data;
+        if (!params.customer_id) {
+            return request.reject(400, 'Customer Inválido');
+        }
+        if (!params.items || params.items?.lenght === 0) {
+            return request.reject(400, "Itens Inválidos");
+        }
+        const customerQuery = SELECT.one.from('sales.customers').where({ id: params.customer_id });
+        const customer = await cds.run(customerQuery);
+        if (!customer) {
+            return request.reject(404, 'Customer não encontrado')
+        }
         
-
+    });
 }
